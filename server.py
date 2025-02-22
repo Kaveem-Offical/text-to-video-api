add8dbf8cea855274093ad3ce8dbba2d70041f29
Kaveem-Offical <165812243+Kaveem-Offical@users.noreply.github.com>
2025-02-22 15:18:37 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,4 +7,5 @@ def home():
     return "FFmpeg API is running!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
